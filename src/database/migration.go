93064c8c067e4a30883bba91c088255a909907d0
Brandon Tripp <brandongtripp@gmail.com>
2024-02-16 14:25:11 -0700

    Change migration.go to handle a test/dev flag for chosing to drop dbs

--- src/database/migration.go
+++ src/database/migration.go
@@ -2,15 +2,34 @@ package database
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/Brandon-G-Tripp/ai-language-teacher/src/database/models"
 )
 
-func AutoMigrate() error {
-    db, err := ConnectDB("../../.env")
+func Migrate(envPath string) error {
+    var path string
+    if envPath == "test" {
+        path = TEST_DB_PATH
+    } else {
+        path = DEV_DB_PATH
+    } 
+
+    db, err := ConnectDB(path)
     if err != nil {
         return fmt.Errorf("Failed to connect to database: %w", err)
     } 
-    db.AutoMigrate(&models.User{})
+
+    // Determine if this is a test env
+    isTestEnv := strings.Contains(envPath, "test")
+
+    if isTestEnv {
+        // Drop table if it exists (only in test env)
+        db.Migrator().DropTable(&models.User{})
+    } 
+
+    // Create table (in all envs) 
+    db.Migrator().CreateTable(&models.User{})
+
     return nil
 }
