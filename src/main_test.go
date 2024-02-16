a3dd4807e331ce1570a711d7084e23aedb3456b7
Brandon Tripp <brandongtripp@gmail.com>
2024-02-16 16:45:52 -0700

    Add loading of dev env variables to main_test.go

--- src/main_test.go
+++ src/main_test.go
@@ -8,10 +8,16 @@ import (
 	"gorm.io/gorm/logger"
 
 	"github.com/Brandon-G-Tripp/ai-language-teacher/src/database"
+	"github.com/joho/godotenv"
 )
 
 
 func TestMain(m *testing.M) {
+    err := godotenv.Load(".env.test")
+    if err != nil {
+        log.Fatalf("Failed to load environment variables: %v", err)
+    } 
+
     db, err := database.ConnectDB("test")
     if err != nil {
         log.Fatalf("Failed to connect to database: %v", err)
