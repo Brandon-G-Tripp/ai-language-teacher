6a9ef10956ccd3cf34205e554e84cd05829b8e96
Brandon Tripp <brandongtripp@gmail.com>
2024-02-16 16:45:08 -0700

    Remove dev/test env logic from orm. Extracted to callers

--- src/database/orm.go
+++ src/database/orm.go
@@ -4,28 +4,11 @@ import (
 	"fmt"
 	"os"
 
-    "github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-const TEST_DB_PATH = "../.env.test"
-const DEV_DB_PATH = "../../.env"
-
 func ConnectDB(envPath string) (*gorm.DB, error) {
-    // Load .env
-    var path string
-    if envPath == "test" {
-        path = TEST_DB_PATH
-    } else {
-        path = DEV_DB_PATH
-    } 
-
-
-    err := godotenv.Load(path)
-    if err != nil {
-        return nil, fmt.Errorf("Failed to load environment variables: %w", err)
-    } 
 
     connectionString := fmt.Sprintf(
         "host=%s port=%s user=%s password=%s dbname=%s",
