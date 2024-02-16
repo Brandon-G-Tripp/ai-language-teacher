7946e41f5c62ff389e5c360650110571108f0e5c
Brandon Tripp <brandongtripp@gmail.com>
2024-02-16 08:52:56 -0700

    Update of orm.go function connectDB to return a db, err tuple

--- src/database/orm.go
+++ src/database/orm.go
@@ -4,23 +4,32 @@ import (
 	"fmt"
 	"os"
 
+    "github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func connectDB() *gorm.DB {
+func connectDB() (*gorm.DB, error) {
+    // Load .env
+    err := godotenv.Load("../../.env")
+    if err != nil {
+        return nil, fmt.Errorf("Failed to load environment variables: %w", err)
+    } 
+
     connectionString := fmt.Sprintf(
-        "host=%s user=%s password=%s dbname=%s",
+        "host=%s port=%s user=%s password=%s dbname=%s",
         os.Getenv("PG_HOST"),
         os.Getenv("PG_PORT"),
         os.Getenv("PG_USER"),
         os.Getenv("PG_PASSWORD"),
         os.Getenv("PG_DB"),
     )
+
+    fmt.Print(connectionString)
     db, err := gorm.Open(postgres.Open(connectionString), &gorm.Config{})
 
     if err != nil {
         panic("failed to connect to database")
     } 
-    return db
+    return db, nil
 } 
