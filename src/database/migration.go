eafa8ab28e4a96586d2760cc7db31ef2ff71510d
Brandon Tripp <brandongtripp@gmail.com>
2024-02-16 08:51:54 -0700

    AutoMigrate function created to migrate to postgres on app launch

--- src/database/migration.go
+++ src/database/migration.go
@@ -1,6 +1,16 @@
 package database
 
-func AutoMigrate() {
-    db := connectDB()
-    db.AutoMigrate(&User{})
+import (
+	"fmt"
+
+	"github.com/Brandon-G-Tripp/ai-language-teacher/src/database/models"
+)
+
+func AutoMigrate() error {
+    db, err := connectDB()
+    if err != nil {
+        return fmt.Errorf("Failed to connect to database: %w", err)
+    } 
+    db.AutoMigrate(&models.User{})
+    return nil
 }
