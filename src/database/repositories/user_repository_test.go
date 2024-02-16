1459e6ce78d11b6df2b371591c9ac3b95aec22ba
Brandon Tripp <brandongtripp@gmail.com>
2024-02-16 10:27:29 -0700

    Initial test for creating UserRepository and creating user

--- src/database/repositories/user_repository_test.go
+++ src/database/repositories/user_repository_test.go
@@ -1,13 +1,59 @@
 package repositories
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/Brandon-G-Tripp/ai-language-teacher/src/database"
+	"github.com/Brandon-G-Tripp/ai-language-teacher/src/database/models"
+) 
 
 func TestCreateUser(t *testing.T) {
     // Arrange 
+    db, err := database.ConnectDB("../../.env.test")
+    if err != nil {
+        t.Fatalf("Error connecting to the database: %v", err)
+    } 
+
+    // deferred cleanup
+    sqlDB, err := db.DB()
+    defer func() {
+        if err != nil {
+            t.Errorf("Failed to close database connection: %v", err)
+        } 
+
+        err = sqlDB.Close()
+        if err != nil {
+            t.Errorf("Failed to close the database connection: %v", err)
+        } 
+    }()
+
+    // init User Repo
+    repo := NewUserRepository(db)
+
+    user := models.User{
+        Name: "John Doe",
+        Email: "john@doe.com",
+        Password: "password",
+    } 
 
     // Act
 
-    // Assert
+    err = repo.Create(&user)
 
+    // Assert
+    if err != nil {
+        t.Fatal("Error creating the user: %v", err)
+    } 
+
+    // verify user was created
+    var createdUser models.User
+    err = repo.GetByEmail(user.Email, &createdUser)
+    if err != nil {
+        t.Fatalf("Error getting user by email: %v", err)
+    } 
+
+    if createdUser.ID == 0 {
+        t.Errorf("Expected non-zero ID for created user")
+    } 
 
 } 
