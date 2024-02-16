f08a2b33608b9410bf3b4ceda373e737064fa580
Brandon Tripp <brandongtripp@gmail.com>
2024-02-16 14:27:15 -0700

    Update main.go to call Migrate wrapper around gorm migration functions

--- src/main.go
+++ src/main.go
@@ -3,8 +3,9 @@ package main
 import "github.com/gin-gonic/gin"
 import "github.com/Brandon-G-Tripp/ai-language-teacher/src/database"
 
+
 func main() {
-    database.AutoMigrate()
+    database.Migrate("dev")
 
     r := gin.Default()
 
