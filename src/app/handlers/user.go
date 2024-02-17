e5e314c3d12b83a61e927eaf5199df5f3b0b0ae9
Brandon Tripp <brandongtripp@gmail.com>
2024-02-17 10:55:05 -0700

    Stubbed out SignUp handler

--- src/app/handlers/user.go
+++ src/app/handlers/user.go
@@ -1,1 +1,37 @@
 package handlers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SignUp(c *gin.Context) {
+    // parse request
+    var input SignUpRequest
+
+    if err := c.ShouldBindJSON(&input); err != nil {
+        // return error 
+    }
+
+    // Todo: Validate input
+
+    // Todo: create user 
+    // call to repo create
+
+    // Generate token 
+
+    // return user
+
+    // stub of response
+    response := SignUpResponse{
+        User: User{
+            ID: 1,
+            Name: "John Doe",
+            Email: input.Email,
+        },
+        Token: "stub-token",
+    }
+
+    c.JSON(http.StatusOK, response)
+} 
