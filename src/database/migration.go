f5c1aa56176fc0e325429af55970f8b0718bfb1b
Brandon Tripp <brandongtripp@gmail.com>
2024-02-16 19:05:14 -0700

    Refactor parameters for migration to take string declaring env

--- src/database/migration.go
+++ src/database/migration.go
@@ -2,28 +2,19 @@ package database
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/Brandon-G-Tripp/ai-language-teacher/src/database/models"
 )
 
-func Migrate(envPath string) error {
-    var path string
-    if envPath == "test" {
-        path = TEST_DB_PATH
-    } else {
-        path = DEV_DB_PATH
-    } 
+func Migrate(env string) error {
 
-    db, err := ConnectDB(path)
+    db, err := ConnectDB(env)
     if err != nil {
         return fmt.Errorf("Failed to connect to database: %w", err)
     } 
 
-    // Determine if this is a test env
-    isTestEnv := strings.Contains(envPath, "test")
 
-    if isTestEnv {
+    if env == "test" {
         // Drop table if it exists (only in test env)
         db.Migrator().DropTable(&models.User{})
     } 
