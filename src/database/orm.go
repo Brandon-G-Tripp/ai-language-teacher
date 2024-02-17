b83719109fb59df5c4477c8972933b7d7454fa26
Brandon Tripp <brandongtripp@gmail.com>
2024-02-16 19:05:44 -0700

    WIP: refactor orm to connect to db based on env passed in and load env vars for that env, May restructure to load the current env from .testenv now that that godotenv works

--- src/database/orm.go
+++ src/database/orm.go
@@ -2,22 +2,36 @@ package database
 
 import (
 	"fmt"
-	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+
+    "github.com/Brandon-G-Tripp/ai-language-teacher/env"
 )
 
-func ConnectDB(envPath string) (*gorm.DB, error) {
+func ConnectDB(envStr string) (*gorm.DB, error) {
 
-    connectionString := fmt.Sprintf(
-        "host=%s port=%s user=%s password=%s dbname=%s",
-        os.Getenv("PG_HOST"),
-        os.Getenv("PG_PORT"),
-        os.Getenv("PG_USER"),
-        os.Getenv("PG_PASSWORD"),
-        os.Getenv("PG_DB"),
-    )
+    var connectionString string
+
+    if envStr == "test" {
+        connectionString = fmt.Sprintf(
+            "host=%s port=%s user=%s password=%s dbname=%s",
+            env.PG_HOST_TEST,
+            env.PG_PORT_TEST,
+            env.PG_USER_TEST,
+            env.PG_PASSWORD_TEST,
+            env.PG_DB_TEST,
+        )
+    } else {
+        connectionString = fmt.Sprintf(
+            "host=%s port=%s user=%s password=%s dbname=%s",
+            env.PG_HOST,
+            env.PG_PORT,
+            env.PG_USER,
+            env.PG_PASSWORD,
+            env.PG_DB,
+        )
+    }
 
     fmt.Print(connectionString)
     db, err := gorm.Open(postgres.Open(connectionString), &gorm.Config{})
