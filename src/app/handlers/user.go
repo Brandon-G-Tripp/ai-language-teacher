681e0961cc4c41eb335eda60960a145be2a9c6b1
Brandon Tripp <brandongtripp@gmail.com>
2024-02-19 11:25:36 -0700

    Update of User handler, check valid password and if password exists, HashPassword call, jwt stub

--- src/app/handlers/user.go
+++ src/app/handlers/user.go
@@ -1,37 +1,78 @@
 package handlers
 
 import (
+	"hash"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
+	"github.com/Brandon-G-Tripp/ai-language-teacher/src/database"
+
+	handler_models "github.com/Brandon-G-Tripp/ai-language-teacher/src/app/models"
+	db_models "github.com/Brandon-G-Tripp/ai-language-teacher/src/database/models"
+	user_repo "github.com/Brandon-G-Tripp/ai-language-teacher/src/database/repositories"
 )
 
 func SignUp(c *gin.Context) {
     // parse request
-    var input SignUpRequest
-
-    if err := c.ShouldBindJSON(&input); err != nil {
-        // return error 
+    var req handler_models.SignUpRequest
+    if err := c.ShouldBindJSON(&req); err != nil {
+        c.JSON(400, err)
+        return 
     }
 
-    // Todo: Validate input
+    log.Printf("Request: %+v", req)
 
-    // Todo: create user 
-    // call to repo create
+    // Validate Request
+    if !IsEmailValid(req.Email) {
+        err := handler_models.ApiError{
+            Message: "Invalid email address",
+            Code: http.StatusBadRequest,
+        }
+
+        c.JSON(err.Code, err)
+
+        return 
+    } 
+
+    // cReate user repo 
+    repo := user_repo.NewUserRepository(database.DB)
+
+    // Check for existing email
+    existing_user, _ := repo.GetByEmail(req.Email)
+    if existing_user != nil {
+        c.JSON(400, "Email already exists")
+        return 
+    } 
+
+    // Hash Password 
+    hashed, err := HashPassword(req.Password)
+    if err != nil {
+        c.JSON(500, err)
+        return 
+    } 
+
+    // Create user 
+    user := db_models.User{
+        Name: req.Name, 
+        Email: req.Email,
+        Password: hashed,
+    }
+    if err := repo.Create(&user); err != nil {
+        c.JSON(500, err)
+        return 
+    } 
 
     // Generate token 
+    token, err := jwt.GenerateToken(user.ID)
+    if err != nil {
+        c.JSON(500, err)
+        return 
+    } 
 
-    // return user
-
-    // stub of response
-    response := SignUpResponse{
-        User: User{
-            ID: 1,
-            Name: "John Doe",
-            Email: input.Email,
-        },
-        Token: "stub-token",
-    }
-
-    c.JSON(http.StatusOK, response)
+    c.JSON(http.StatusOK, handler_models.SignUpResponse{
+        User: user, 
+        Token: token,
+    })
 } 
