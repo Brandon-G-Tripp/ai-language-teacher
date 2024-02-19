6cb37e22610a088f04d9450d9027caa12e526eb9
Brandon Tripp <brandongtripp@gmail.com>
2024-02-19 14:57:20 -0700

    Generate and Validate functions for jwt

--- src/app/handlers/jwt_tokens.go
+++ src/app/handlers/jwt_tokens.go
@@ -1,9 +1,13 @@
 package handlers
 
 import (
-	"github.com/Brandon-G-Tripp/ai-language-teacher/env"
-	db_models "github.com/Brandon-G-Tripp/ai-language-teacher/src/database/models"
+	"errors"
+
 	"github.com/golang-jwt/jwt/v5"
+
+	"github.com/Brandon-G-Tripp/ai-language-teacher/env"
+
+	db_models "github.com/Brandon-G-Tripp/ai-language-teacher/src/database/models"
 )
 
 func GenerateToken(user *db_models.User) (string, error) {
@@ -17,4 +21,32 @@ func GenerateToken(user *db_models.User) (string, error) {
     claims["email"] = user.Email
 
     return token.SignedString(secretKey)
+} 
+
+// Validate token for later
+
+func ValidateToken(tokenString string) error {
+    token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+        return []byte(env.JWT_SECRET), nil
+    })
+    if err != nil {
+        return errors.New("Error parsing token")
+    } 
+
+    claims, ok := token.Claims.(jwt.MapClaims)
+    if !ok {
+        return errors.New("invalid claims")
+    } 
+
+    idFloat, ok := claims["id"].(float64)
+    if !ok {
+        return errors.New("invalid id claim")
+    }
+    id := uint(idFloat)
+    if id == 0 {
+        return errors.New("invalid id claim: Equals 0")
+    }
+
+    return nil
+
 } 
