e3533a1af98b42634c747d169e6a32ce0c5ecd69
Brandon Tripp <brandongtripp@gmail.com>
2024-03-01 13:14:36 -0700

    Add req and resp structs for login and logout in handlers, update signup req and resp

--- src/app/models/user.go
+++ src/app/models/user.go
@@ -5,12 +5,30 @@ import (
 )
 
 type SignUpRequest struct {
-    Name string
-    Email string
-    Password string
+    Name string `json:"name"`
+    Email string `json:"email"`
+    Password string `json:"password"`
 } 
 
 type SignUpResponse struct {
-    User db_models.User
-    Token string
+    User *db_models.User `json:"user"`
+    Token string `json:"token"`
 } 
+
+type LoginRequest struct {
+    Email string `json:"email"`
+    Password string `json:"password"`
+} 
+
+type LoginResponse struct {
+    User *db_models.User `json:"user"`
+    Token string `json:"token"`
+} 
+
+type LogoutRequest struct {
+    Token string `json:"token"`
+} 
+
+type LogoutResponse struct {
+    Message string `json:"message"`
+}
